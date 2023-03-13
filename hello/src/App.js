47a8b17e504e4ebe9217110abccf45e71249047e
Yeboah Godfred <ygodfred3@gmail.com>
2023-03-13 06:51:39 +0000

    Still on tailwind

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,20 +1,24 @@
 import './index.css';
 import Element from './components/Element';
-import { useState } from 'react';
 
 function App() {
-  const [pose , setpose] = useState("");
   const user_available = true;
   return (
     <div className="App bg-green-600 m-3 p-4">
-      <input type='text' onChange={(e) =>{
-        setpose(e.target.value);
-      }}></input>
       {
         user_available ? 
         <>
-        <Element name = "Godfred is " position = {pose}/>
-        <Element name = "Kelvin is " position = "a mobile developer"/>
+          <div className='flex flex-wrap'>
+            <Element name = "Andre " position = "React developer" image = "https://images.unsplash.com/photo-1677261905060-7a93f54682fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Mash " position = "mobile developer" image = "https://images.unsplash.com/photo-1678101629507-10cb3ba27fd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Ortega " position = "software engineer " image = "https://images.unsplash.com/photo-1677700674613-4ba4a1bb026d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Sally " position = "React developer" image = "https://images.unsplash.com/photo-1677537946723-86a6d81741b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Jerome " position = "mobile developer" image = "https://images.unsplash.com/photo-1677261905060-7a93f54682fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Brant " position = "software engineer" image = "https://images.unsplash.com/photo-1678101629507-10cb3ba27fd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Drill " position = "React developer" image = "https://images.unsplash.com/photo-1677700674613-4ba4a1bb026d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Kent " position = "mobile developer" image = "https://images.unsplash.com/photo-1677537946723-86a6d81741b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+            <Element name = "Frank " position = "software engineer" image = "https://images.unsplash.com/photo-1677261905060-7a93f54682fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8UzRNS0xBc0JCNzR8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"/>
+          </div>
         </>
         :
         <h2>User not registered</h2>
--- hello/src/components/Element.jsx
+++ hello/src/components/Element.jsx
@@ -1,7 +1,22 @@
 function element(props){
     return(
         <>
-            <h2>{props.name} <i>{props.position}</i></h2>
+            <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img class="block mx-auto h-24 rounded-full  sm:mx-0 sm:shrink-0" 
+                src={props.image}/>
+                <div class="text-center space-y-2 sm:text-left">
+                    <div class="space-y-0.5">
+                        <p class="text-lg text-black font-semibold">
+                             {props.name}
+                        </p>
+                        <p class="text-slate-500 font-medium">
+                            {props.position}
+                        </p>
+                    </div>
+                <button class="px-4 py-1 text-sm text-blue-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-blue-600
+                 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+                </div>
+            </div>
         </>
     )
 }
